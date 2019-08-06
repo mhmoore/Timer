7b95da9d7b32501b2862c9078ebe9d78869bf0cf
Mike Moore <michaelhmoore1@gmail.com>
2019-08-06 09:58:52 -0600

    initial commit

---
+++ AppTimer/AppTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTimer
+//
+//  Created by Michael Moore on 8/6/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
